30880f9ef803d9876541b3ca662aea063576fde5
Raúl Prieto García <raprieto@unirioja.es>
2023-03-13 23:29:11 +0100

    a

--- app/src/main/java/com/example/racetracker/MainActivity.kt
+++ app/src/main/java/com/example/racetracker/MainActivity.kt
@@ -30,7 +30,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
